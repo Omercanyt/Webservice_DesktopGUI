39138b928232d61df99fa65d09141f3d5be2ebc7
Günseli Sevinç <51919213+gunselisevinc@users.noreply.github.com>
2021-06-20 22:07:13 +0300

    Computers attributes created

--- src/sample/Computers.java
+++ src/sample/Computers.java
@@ -1,4 +1,12 @@
 package sample;
 
 public class Computers {
+    private Double computerPrice;
+    private String computerScreenSize;
+    private String computerModel;
+    private String computerBrand;
+    private String screenResolution;
+    private String processor;
+    private String memory;
+    private String storageCapacity;
 }

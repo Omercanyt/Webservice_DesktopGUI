410181b0d91f2d0c0fe58f6c03f1a6609c66a928
Günseli Sevinç <51919213+gunselisevinc@users.noreply.github.com>
2021-06-20 22:16:16 +0300

    Product Table declared to Controller class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
 
 public class Controller {
 
@@ -25,4 +26,6 @@ public class Controller {
     private Button sortPriceButton;
     @FXML
     private Button compareButton;
+    @FXML
+    private TableView<Object> productListView;
 }

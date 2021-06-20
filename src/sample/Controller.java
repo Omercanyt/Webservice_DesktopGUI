1aa33b8edc425d07cc3d7076c41f85b00a456d41
Günseli Sevinç <51919213+gunselisevinc@users.noreply.github.com>
2021-06-20 22:14:56 +0300

    Tabs declared to Controller class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
 public class Controller {
+
+    @FXML
+    private Tab phoneTab;
+    @FXML
+    private Tab computerTab;
+
 }

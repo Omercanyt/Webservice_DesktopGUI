67e32d498767b1316572ccfd764f5fec58fb4146
Günseli Sevinç <51919213+gunselisevinc@users.noreply.github.com>
2021-06-20 22:15:20 +0300

    Comboboxes declared to Controller class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 
 public class Controller {
@@ -9,5 +10,12 @@ public class Controller {
     private Tab phoneTab;
     @FXML
     private Tab computerTab;
-
+    @FXML
+    private ComboBox<String> phoneModelComboBox;
+    @FXML
+    private ComboBox<String>  phoneBrandComboBox;
+    @FXML
+    private ComboBox<String>  computerModelComboBox;
+    @FXML
+    private ComboBox<String>  computerBrandComboBox;
 }

78c6b649b0556ca98879f4fe4952cb9b33f33ed1
Günseli Sevinç <51919213+gunselisevinc@users.noreply.github.com>
2021-06-20 22:07:51 +0300

    Computers constructors are implemented

--- src/sample/Computers.java
+++ src/sample/Computers.java
@@ -9,4 +9,18 @@ public class Computers {
     private String processor;
     private String memory;
     private String storageCapacity;
+
+    public Computers() {
+    }
+
+    public Computers(Double computerPrice, String computerScreenSize, String computerModel, String computerBrand, String screenResolution, String processor, String memory, String storageCapacity) {
+        this.computerPrice = computerPrice;
+        this.computerScreenSize = computerScreenSize;
+        this.computerModel = computerModel;
+        this.computerBrand = computerBrand;
+        this.screenResolution = screenResolution;
+        this.processor = processor;
+        this.memory = memory;
+        this.storageCapacity = storageCapacity;
+    }
 }
